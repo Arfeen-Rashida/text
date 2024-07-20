7cb91289d57e8b44fd34c0da07b09f241edb4787
Rashida-Arfeen <rashidaarfeen@gmail.com>
2024-07-20 20:13:48 +0530

    Revert "text"
    This reverts commit f0539d25a1f1ae36d56841952501048b61855562.

--- module1.mjs
+++
@@ -1,5 +0,0 @@
-import dz, {a, b, c} from './module2.mjs'
-console.log(dz);
-console.log(b);
-console.log(c);
-console.log(a);
--- module2.mjs
+++
@@ -1,8 +0,0 @@
-const a = "Harry";
-const b = "priya";
-const c = "dev";
-const d = "aakash";
-export default d;
-export {a};
-export {b};
-export {c};
